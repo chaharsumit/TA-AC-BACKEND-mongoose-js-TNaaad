cb67fe04bbc5a7e6c0d0d08b28cf6ab9a0180a1d
Sumit Chahar <chahar.sumit888@gmail.com>
2021-12-06 22:45:16 +0530

    Add min and max lengths for password

--- block-BNaacj/sample/models/user.js
+++ block-BNaacj/sample/models/user.js
@@ -3,6 +3,6 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-  password: { type: String, required: true, min: 5, max: 15},
+  password: { type: String, required: true, minlength: 5, maxlength: 15},
   createAt: { type: Date, default: new Date() }
 }, { timestamps: true });
